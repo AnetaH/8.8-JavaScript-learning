c638e67f003b66811f4029db0a731dc597ecfe95
Aneta Halz <aneta.halz@interia.pl>
2017-02-26 19:53:16 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+animal = animal.toUpperCase();
+
+var textAfter = text.replace('Papugi', animal);
+console.log('text po zmianie: ' + textAfter);
+
+console.log('pierwsza połowa zmienionego textu: ' + textAfter.substr(0, (textAfter.length/2)));
