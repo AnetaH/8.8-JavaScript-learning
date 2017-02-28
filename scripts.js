25ccd7201895ea01c2fe19004854dcd6a8e6c3cf
Aneta Halz <aneta.halz@interia.pl>
2017-02-28 13:03:19 +0100

    correction

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
-animal = animal.toUpperCase();
 
-var textAfter = text.replace('Papugi', animal);
+var textAfter = text.replace('Papugi', animal.toUpperCase());
 console.log('text po zmianie: ' + textAfter);
 
-console.log('pierwsza połowa zmienionego textu: ' + textAfter.substr(0, (textAfter.length/2)));
+console.log('pierwsza połowa zmienionego textu: ' + textAfter.substr(0, textAfter.length / 2));
